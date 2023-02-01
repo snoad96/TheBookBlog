7961ec08a809a66006fb5ae5fe6fc7fd72298e71
DSnoad <danialsnoad@hotmail.com>
2023-02-01 13:37:14 +0000

    Add Members class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Members(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
+
+
+book = models.ImageField(max_length=100)

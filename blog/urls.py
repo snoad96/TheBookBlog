028349fece24803f9c9b63528e8dd1211dec6696
DSnoad <danialsnoad@hotmail.com>
2023-03-23 13:42:43 +0000

    Change from signin to login.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.welcome, name='welcome'),
     path('blog/', views.postlist, name='postlist'),
-    path('signin/', views.signin, name='signin'),
+    path('login/', views.signin, name='login'),
+    path('accounts/', include("django.contrib.auth.urls")),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,6 @@ def postlist(request):
     queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/postlist.html', {'posts': queryset})
 
-def signin(request):
-    return render(request, 'blog/signin.html')
+
+def login(request):
+    return render(request, 'blog/login.html')

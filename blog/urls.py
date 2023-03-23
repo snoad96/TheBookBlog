bde7fa78f0392a02de8066551dc49da9cc648119
DSnoad <danialsnoad@hotmail.com>
2023-03-23 13:30:38 +0000

    add signin page to urls and views

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('blog/', views.postlist, name='postlist'),
+    path('signin/', views.signin, name='signin'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,6 @@ def welcome(request):
 def postlist(request):
     queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/postlist.html', {'posts': queryset})
+
+def signin(request):
+    return render(request, 'blog/signin.html')
